a8fa49e76953ebf09d686b6904c06be5fb798bb0
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-06-18 00:57:28 +0900

    style: フラッシュメッセージの日本語化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -28,7 +28,7 @@ class CatsController < ApplicationController
     @cat = Cat.new(cat_params)
 
     if @cat.save
-      redirect_to @cat, notice: "Cat was successfully created."
+      redirect_to @cat, notice: "猫を登録しました。"
     else
       render :new, status: :unprocessable_entity
     end
@@ -37,7 +37,7 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "Cat was successfully updated.", status: :see_other
+      redirect_to @cat, notice: "猫を更新しました。", status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -46,7 +46,7 @@ class CatsController < ApplicationController
   # DELETE /cats/1
   def destroy
     @cat.destroy!
-    redirect_to cats_url, notice: "Cat was successfully destroyed.", status: :see_other
+    redirect_to cats_url, notice: "猫を削除しました。", status: :see_other
   end
 
   private
