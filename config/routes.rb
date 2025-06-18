c6d6fd31392bbf5e1d24938a02af7539037692d2
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-06-18 22:44:51 +0900

    feat: scaffoldで作成したルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :owls
+  resources :hedgehogs
+  resources :chicks
+  resources :dogs
   resources :cats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,5 +11,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: redirect("/cats")
 end
